d7a0733b3241a9581587c43a6acdfa230616e35f
Avinash Kadaji <kadaji@gmail.com>
2025-04-10 14:31:18 -0400

    form field other college attended

--- form_fields/fields/__init__.py
+++ form_fields/fields/__init__.py
@@ -3,5 +3,6 @@ from .read_only import ReadOnlyField, ReadOnlyWidget
 from .signature import SignatureField, SignatureWidget
 from .graduation_month_year import GraduationMonthYearWidget
 from .security_question import SecurityQuestionField, SecurityQuestionWidget
+from .other_college_attended import OtherCollegeAttendedField, OtherCollegeAttendedWidget
 
 from .validators import validate_security_question
