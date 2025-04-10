695414a66514721b6f0865fb73133e14d864fe2c
Avinash Kadaji <kadaji@gmail.com>
2025-04-10 14:32:59 -0400

    other college q

---
+++ form_fields/fields/other_college_attended.py
@@ -0,0 +1,51 @@
+
+import copy
+import datetime
+import warnings
+from collections import defaultdict
+from itertools import chain
+
+from django.forms import widgets
+from django.forms.widgets import Select
+
+from django.forms.utils import to_current_timezone
+from django.templatetags.static import static
+from django.utils import datetime_safe, formats
+from django.utils.datastructures import OrderedSet
+from django import forms
+from django.utils.dates import MONTHS
+from django.utils.formats import get_format
+from django.utils.html import format_html, html_safe
+from django.utils.regex_helper import _lazy_re_compile
+from django.utils.safestring import mark_safe
+from django.utils.topological_sort import (
+    CyclicDependencyError, stable_topological_sort,
+)
+from django.utils.translation import gettext_lazy as _
+
+
+class OtherCollegeAttendedWidget(widgets.MultiWidget):
+    """
+    A widget that asks security questions and 2 verification answers.
+    """
+
+    template_name = 'widgets/other_college_attended.html'
+
+    def __init__(self, attrs=None, questions=[], mode=0):
+        _widgets = (
+            widgets.TextInput(attrs=attrs),
+            widgets.TextInput(attrs=attrs),
+            widgets.TextInput(attrs=attrs),
+        )
+
+        super().__init__(_widgets, attrs)
+
+    def decompress(self, value):
+        if value:
+            print(value)
+            return ['1', '2', '3']
+    
+        return [None, None, None]
+
+class OtherCollegeAttendedField(forms.CharField):
+    widget = OtherCollegeAttendedWidget
